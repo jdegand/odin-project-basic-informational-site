a22ac5c4d4f9f5afac2f22848287d097506b3814
jdegand <70610011+jdegand@users.noreply.github.com>
2021-01-14 17:24:08 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+//import file system fs
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    const url = req.url;
+
+    if(url === '/') {
+        res.write('Home Page'); //fs readfile index.html
+        res.end();
+    } else if(url === '/about') {
+        res.write('About Page');
+        res.end();
+    }else if(url === '/contact-me') {
+        res.write('Contact Page');
+        res.end();
+    } else {
+        res.write('404 Page');
+        res.end();
+    }
+}).listen(8080, function() {
+    console.log("Server is listening on 8080");
+});
+
+//node index.js
